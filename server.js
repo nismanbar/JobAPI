300bc2ab550ab3696895cb38573e44d3993fc0ec
nismanbar <nismanbar@gmail.com>
2026-04-21 15:29:23 +0300

    dvscx

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-const http = require("http");
-require("dotenv").config();
-const app = require("./app");
-
-const port = process.env.PORT || 3000;
+const http = require('http');
+const app = require('./app');
+const port = 3000; 
 const server = http.createServer(app);
-
-server.listen(port, "0.0.0.0", () => {
-  console.log(`Server is running on port ${port}`);
+server.listen(port,'0.0.0.0',()=>{
+console.log("Server is Runing"); 
 });
