5709d6c153f21e706ac6e5b49c83b21a96adda96
nismanbar <nismanbar@gmail.com>
2026-04-28 12:01:48 +0300

    dasdsa

--- app.js
+++ app.js
@@ -39,13 +39,16 @@ app.use("/job", JobRouter);
 const CompanyRouter = require('./api/routes/companyRoutes');
 app.use("/company", CompanyRouter);
 
+const ChatRouter = require('./api/routes/chatRoutes');
+app.use("/chat", ChatRouter);
+
 // 404
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
   res.status(404).json({ message: "Route not found" });
 });
 
 // Error handler
-app.use((err, req, res, next) => {
+app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500).json({ message: "Server error", error: err.message });
 });
